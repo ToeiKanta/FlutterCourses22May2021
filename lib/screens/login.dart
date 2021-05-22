89def311c01e5a2cbd6cbd2e160fa2f4cc7c0c84
MuyoNz <pattaraponler@gmail.com>
2021-05-22 14:10:35 +0700

    need help text and language flag

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -23,17 +23,85 @@ class _LoginScreenState extends State<LoginScreen> {
         width: Get.width,
         // column screen
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            // top widget
+            Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    top: Get.height * 0.1,
+                  ),
+                  // column logo
+                  child: logo(),
+                ),
+                Container(
+                  width: Get.width * 0.8,
+                  height: 44,
+                  margin: EdgeInsets.only(
+                    top: Get.height * 0.1,
+                  ),
+                  child: loginButton(),
+                ),
+              ],
+            ),
             Container(
-              margin: EdgeInsets.only(
-                top: Get.height * 0.1,
+              margin: EdgeInsets.symmetric(horizontal: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // left widget - need help
+                  Row(
+                    children: [
+                      Text(
+                        "ต้องการความช่วยเหลือ ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        "คลิก",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    "assets/thai-flag.png",
+                    width: 24,
+                  )
+                ],
               ),
-              // column logo
-              child: logo(),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  ElevatedButton loginButton() {
+    return ElevatedButton(
+      child: Text(
+        "เข้าใช้งาน",
+        style: TextStyle(
+          color: AppColors.primary,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.all(Colors.yellow.shade700),
+      ),
+      onPressed: () {},
     );
   }
 
