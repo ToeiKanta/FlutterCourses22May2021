6d51e83fad3f243c832ca07081d7b68f853655bb
Boonsong Srithong <song@odds.team>
2021-05-23 09:46:25 +0700

    move login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example_flutter1/screens/login.dart';
+import 'package:example_flutter1/screens/Login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
