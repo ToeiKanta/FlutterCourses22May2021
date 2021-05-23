17ea7ca99b89d5ff47806b392f923b91b539ab70
Kantapong Poonaksem <toeikanta@gmail.com>
2021-05-23 15:17:40 +0700

    add unit test

--- test/screens/login/login_controller_test.dart
+++ test/screens/login/login_controller_test.dart
@@ -3,9 +3,26 @@ import 'package:example_flutter1/screens/login/login_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
-  test("should retrun flag th", () {
+  group("test getImagePathLanguage function", () {
     var controller = LoginController();
-    var actual = controller.getImagePathLanguage(Language.th);
-    expect(actual, "assets/thai-flag.png");
+    test("should retrun flag th", () {
+      var actual = controller.getImagePathLanguage(Language.th);
+      expect(actual, "assets/thai-flag.png");
+    });
+    test("should retrun flag en", () {
+      var actual = controller.getImagePathLanguage(Language.en);
+      expect(actual, "assets/eng-flag.png");
+    });
+  });
+  group("test setLanguageSelected function", () {
+    var controller = LoginController();
+    test("check languageSelected.value = th", () {
+      controller.setLanguageSelected(Language.th);
+      expect(controller.languageSelected.value, Language.th);
+    });
+    test("check languageSelected.value = en", () {
+      controller.setLanguageSelected(Language.en);
+      expect(controller.languageSelected.value, Language.en);
+    });
   });
 }
